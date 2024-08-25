fa3288ef456235b8eee220ed02ca34a8d29b50c5
Daly Bouazizi <bouazizimohamedali@isimsf.u-sfax.tn>
2024-08-25 18:55:32 +0100

    aa

--- Projet-Stage/Services/Classes/EmailSenderService.cs
+++ Projet-Stage/Services/Classes/EmailSenderService.cs
@@ -29,7 +29,7 @@ public class EmailSenderService : BackgroundService
                 await emailService.SendContractsEmailAndAddAlertAsync();
             }
 
-            await Task.Delay(1000000, stoppingToken);
+            await Task.Delay(30000000, stoppingToken);
         }
     }
 }
